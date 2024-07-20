98304e38678ebe838ac61627ff50831c6b92b451
Diogo Rodrigo Pedreira Galvão <diogo.rgalvao@outlook.com>
2024-07-19 22:13:39 -0300

    feat: Criação da página inicial do aplicativo

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
+import Pagina from "@/components/templates/Pagina";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Pagina>
+      <Text style={{ fontFamily: "SPACEMISSION", color: "white" }}>
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </Pagina>
   );
 }
