6f6656b32452de9bfb62ec94f051db2b0fe20434
Diogo Rodrigo Pedreira Galvão <diogo.rgalvao@outlook.com>
2024-07-19 21:57:12 -0300

    feat: Implementação do modelo de perguntas (mocks)

---
+++ data/constants/PerguntasApp.ts
@@ -0,0 +1,206 @@
+import PerguntasProps from "../model/Pergunta";
+
+const PerguntasApp: PerguntasProps[] = [
+  {
+    id: 1,
+    enunciado: "Qual é o maior planeta do nosso sistema solar?",
+    opcoes: ["Terra", "Júpiter", "Saturno", "Marte"],
+    resposta: 1,
+  },
+  {
+    id: 2,
+    enunciado: "Qual planeta é conhecido como o Planeta Vermelho?",
+    opcoes: ["Vênus", "Marte", "Júpiter", "Mercúrio"],
+    resposta: 1,
+  },
+  {
+    id: 3,
+    enunciado: "Qual é o satélite natural da Terra?",
+    opcoes: ["Lua", "Fobos", "Deimos", "Europa"],
+    resposta: 0,
+  },
+  {
+    id: 4,
+    enunciado: "Qual é a estrela mais próxima da Terra?",
+    opcoes: ["Sirius", "Alpha Centauri", "Proxima Centauri", "Sol"],
+    resposta: 3,
+  },
+  {
+    id: 5,
+    enunciado: "Qual planeta é conhecido por seus anéis?",
+    opcoes: ["Júpiter", "Saturno", "Urano", "Netuno"],
+    resposta: 1,
+  },
+  {
+    id: 6,
+    enunciado: "Qual é a galáxia na qual vivemos?",
+    opcoes: ["Andrômeda", "Via Láctea", "Sombrero", "Messier 87"],
+    resposta: 1,
+  },
+  {
+    id: 7,
+    enunciado: "Qual é o planeta mais quente do sistema solar?",
+    opcoes: ["Mercúrio", "Vênus", "Marte", "Júpiter"],
+    resposta: 1,
+  },
+  {
+    id: 8,
+    enunciado: "Qual é o menor planeta do sistema solar?",
+    opcoes: ["Mercúrio", "Marte", "Plutão", "Terra"],
+    resposta: 0,
+  },
+  {
+    id: 9,
+    enunciado: "Qual é a maior lua de Saturno?",
+    opcoes: ["Titã", "Europa", "Ganimedes", "Ío"],
+    resposta: 0,
+  },
+  {
+    id: 10,
+    enunciado: "Quantos planetas existem no nosso sistema solar?",
+    opcoes: ["7", "8", "9", "10"],
+    resposta: 1,
+  },
+  {
+    id: 11,
+    enunciado: "Qual é a distância média entre a Terra e o Sol?",
+    opcoes: [
+      "93 milhões de milhas",
+      "93 mil milhas",
+      "9,3 milhões de milhas",
+      "930 mil milhas",
+    ],
+    resposta: 0,
+  },
+  {
+    id: 12,
+    enunciado: "Qual planeta tem a maior montanha do sistema solar?",
+    opcoes: ["Terra", "Marte", "Vênus", "Júpiter"],
+    resposta: 1,
+  },
+  {
+    id: 13,
+    enunciado: "Quantas luas tem Marte?",
+    opcoes: ["1", "2", "3", "4"],
+    resposta: 1,
+  },
+  {
+    id: 14,
+    enunciado:
+      "Qual é o nome da missão que levou o homem à Lua pela primeira vez?",
+    opcoes: ["Apollo 11", "Apollo 13", "Viking 1", "Curiosity"],
+    resposta: 0,
+  },
+  {
+    id: 15,
+    enunciado: "Qual é o maior satélite natural do sistema solar?",
+    opcoes: ["Titã", "Ganimedes", "Europa", "Luna"],
+    resposta: 1,
+  },
+  {
+    id: 16,
+    enunciado: "Qual é a duração de um dia em Júpiter?",
+    opcoes: ["24 horas", "10 horas", "12 horas", "16 horas"],
+    resposta: 1,
+  },
+  {
+    id: 17,
+    enunciado: "Qual é o planeta mais distante do Sol no nosso sistema solar?",
+    opcoes: ["Urano", "Netuno", "Plutão", "Saturno"],
+    resposta: 1,
+  },
+  {
+    id: 18,
+    enunciado:
+      "Qual planeta tem uma grande mancha vermelha que é uma tempestade?",
+    opcoes: ["Terra", "Saturno", "Júpiter", "Netuno"],
+    resposta: 2,
+  },
+  {
+    id: 19,
+    enunciado: "Qual é o segundo menor planeta do sistema solar?",
+    opcoes: ["Marte", "Mercúrio", "Vênus", "Terra"],
+    resposta: 0,
+  },
+  {
+    id: 20,
+    enunciado: "Qual é a maior lua de Júpiter?",
+    opcoes: ["Europa", "Ganimedes", "Ío", "Calisto"],
+    resposta: 1,
+  },
+  {
+    id: 21,
+    enunciado: "Qual é a composição principal da atmosfera de Vênus?",
+    opcoes: ["Oxigênio", "Hidrogênio", "Dióxido de Carbono", "Metano"],
+    resposta: 2,
+  },
+  {
+    id: 22,
+    enunciado: "Qual é o nome do cinturão de asteroides entre Marte e Júpiter?",
+    opcoes: [
+      "Cinturão de Kuiper",
+      "Cinturão de Van Allen",
+      "Cinturão de Asteroides",
+      "Cinturão de Oort",
+    ],
+    resposta: 2,
+  },
+  {
+    id: 23,
+    enunciado: "Qual planeta tem o dia mais longo em relação ao seu ano?",
+    opcoes: ["Vênus", "Marte", "Júpiter", "Mercúrio"],
+    resposta: 0,
+  },
+  {
+    id: 24,
+    enunciado:
+      "Qual é o nome do maior vulcão do sistema solar, localizado em Marte?",
+    opcoes: ["Monte Olimpo", "Monte Everest", "Mauna Kea", "Kilauea"],
+    resposta: 0,
+  },
+  {
+    id: 25,
+    enunciado: "Qual é a duração de um ano em Mercúrio?",
+    opcoes: [
+      "88 dias terrestres",
+      "365 dias terrestres",
+      "225 dias terrestres",
+      "687 dias terrestres",
+    ],
+    resposta: 0,
+  },
+  {
+    id: 26,
+    enunciado:
+      "Qual é a unidade de medida usada para a distância entre a Terra e outras estrelas?",
+    opcoes: ["Anos-luz", "Quilômetros", "Milhas", "Unidades Astronômicas"],
+    resposta: 0,
+  },
+  {
+    id: 27,
+    enunciado:
+      "Qual é o nome do primeiro satélite artificial lançado ao espaço?",
+    opcoes: ["Explorer 1", "Sputnik 1", "Luna 2", "Vostok 1"],
+    resposta: 1,
+  },
+  {
+    id: 28,
+    enunciado: "Qual planeta é conhecido por ter um grande sistema de anéis?",
+    opcoes: ["Urano", "Júpiter", "Saturno", "Netuno"],
+    resposta: 2,
+  },
+  {
+    id: 29,
+    enunciado: "Qual é o nome da maior lua de Netuno?",
+    opcoes: ["Titã", "Ganimedes", "Tritão", "Europa"],
+    resposta: 2,
+  },
+  {
+    id: 30,
+    enunciado: "Quantas luas têm Júpiter?",
+    opcoes: ["53", "67", "79", "95"],
+    resposta: 2,
+  },
+];
+
+export default PerguntasApp;
---
+++ data/model/Pergunta.ts
@@ -0,0 +1,6 @@
+export default interface PerguntasProps {
+  id: number;
+  enunciado: string;
+  opcoes: string[];
+  resposta: number;
+}
